c0dbb54a9d506ff48703da492ca8ea13420bb622
BGSTA9 <soheilsanatii@gmail.com>
2026-01-19 01:05:10 +0300

    Refactor: Rename author-related constant and field to `developed_by` for consistency.

--- src/welcome.rs
+++ src/welcome.rs
@@ -22,7 +22,7 @@ const LOGO_PARTS: [(&str, &str); 6] = [
 const GITHUB_URL: &str = "https://github.com/BGSTA9/fluxphy";
 
 /// Author Name
-const AUTHOR: &str = "Argo Navis Research Laboratory";
+const DEVELOPED_BY: &str = "Argo Navis Research Laboratory";
 
 /// Info lines to display (Introduction + Metadata)
 /// Returns: (Color, Label, Value, IsItalic)
